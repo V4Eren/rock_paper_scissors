c0794524efa4b226442be46d7bdc7c9bdc7cdd7c
V4Eren <gumpseven64@gmail.com>
2025-11-23 20:36:35 +0300

    Added playRound function. It can now play one round of a game.

--- index.js
+++ index.js
@@ -30,6 +30,42 @@ function getHumanChoice() {
     }
 }
 
-console.log(getComputerChoice())
+let humanScore = 0;
+let computerScore = 0;
+//game logic function: scissors beats paper, paper beats rock, rock beats scissors
+function playRound(humanChoice, computerChoice) {
+    let humanWin = (humanChoice == "rock" && computerChoice == "scissors") ||
+                    (humanChoice == "paper" && computerChoice == "rock") ||
+                    (humanChoice == "scissors" && computerChoice == "paper");
+
+    console.log("Human choice is " + humanWin);                
+    let computerWin = (computerChoice == "rock" && humanChoice == "scissors") ||
+                        (computerChoice == "paper" && humanChoice == "rock") ||
+                        (computerChoice == "scissors" && humanChoice == "paper");
+
+    console.log("Computer choice is " + computerWin);
+
+    if(humanWin) {
+        let resultMessage = "You win! " + humanChoice + " beats " + computerChoice + ".";
+        console.log(resultMessage);
+        humanScore++;
+    }
+
+    if(computerWin) {
+        let resultMessage = "You lose! " + computerChoice + " beats " + humanChoice + ".";
+        console.log(resultMessage);
+        computerScore++;
+    }
+
+    //Draw condition
+    if(!humanWin && !computerWin){
+        console.log("It is draw. No one gets a score.");
+    }
+}
+
+
+computerChoice = getComputerChoice();
 humanChoice = getHumanChoice()
+console.log(computerChoice)
 console.log(humanChoice)
+playRound(humanChoice,computerChoice);
